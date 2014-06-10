0ff4f56da5a7eb1245ebf35968f817646948cd6b
doofmars <jan-preuss@gmx.de>
2014-06-11 00:59:24 +0200

    Furter work on website
    Fixed Resizing issue.
    Added first info text
    Prepared Help page
    Added Black icon
    Prepared font

--- js/fadeSlideShow.js
+++ js/fadeSlideShow.js
@@ -24,7 +24,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 jQuery.fn.fadeSlideShow = function(options) {
 	return this.each(function(){
 		settings = jQuery.extend({
-     		width: 450, // default width of the slideshow
+     		width: 300, // default width of the slideshow
 			speed: 'slow', // default animation transition speed
 			interval: 10000, // default interval between image change
 			ListElement: 'fssList', // default id for image / content controll list
