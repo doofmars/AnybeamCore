5750da7b8fe22de7f23b9e43719d419a76f2aab6
doofmars <jan-preuss@gmx.de>
2014-06-12 16:49:46 +0200

    Fixed responsive problems
    Added help topics
    Added offstet scroll navigation

---
+++ js/navigation.js
@@ -0,0 +1,7 @@
+
+	$(".nav-item").click( function() {
+		console.log("dst");
+	    var location = $(this).attr("href");
+	    $("html, body").animate({scrollTop: $(location).offset().top - 60 }, 'fast');
+	    return false;
+	});
