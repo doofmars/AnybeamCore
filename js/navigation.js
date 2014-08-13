6cc1408432af27bb7aab70eef5d2b20adfafc222
doofmars <jan-preuss@gmx.de>
2014-08-14 01:50:11 +0200

    Replaced Slideshow
    Replaced fadeSlideShow with jssor (http://www.jssor.com/)

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,8 @@
+//Function for smooth scrolling
 
-	$(".nav-item").click( function() {
-		console.log("dst");
-	    var location = $(this).attr("href");
-	    $("html, body").animate({scrollTop: $(location).offset().top - 60 }, 'fast');
-	    return false;
-	});
+$(".nav-item").click( function() {
+	console.log("dst");
+	var location = $(this).attr("href");
+	$("html, body").animate({scrollTop: $(location).offset().top - 60 }, 'fast');
+	return false;
+});
